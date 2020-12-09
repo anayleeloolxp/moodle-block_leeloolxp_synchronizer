b931287ee5c7336e2d37d332b07db66464bbeb0a
Anay Infowind <anay.infowind@gmail.com>
2020-12-09 16:05:40 +0530

    Added sync cats function

--- block_leeloolxp_synchronizer.php
+++ block_leeloolxp_synchronizer.php
@@ -15,141 +15,255 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Renderer for block leeloolxp_synchronizer
+ * class block_leeloolxp_tracking
  *
  * @package    block_leeloolxp_synchronizer
- * @copyright  2020 @ leelolxp.com
+ * @copyright  2020 leeloolxp.com
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-defined('MOODLE_INTERNAL') || die;
-require_once( $CFG->dirroot.'/course/lib.php' );
+
+require_once($CFG->dirroot.'/course/lib.php');
 
 /**
- * Block sync info
+
+ * class block_leeloolxp_synchronizer
+
  *
- * @copyright  2020 Leeloo LXP (https://leeloolxp.com)
+
+ * @package    block_leeloolxp_synchronizer
+ * 
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class block_leeloolxp_synchronizer extends block_base {
-    /**
-     * Show attendance information of user.
-     */
+
+   
+    protected $timestart = null;
 
     /**
-     * Block initialization
+     * Initialises the block
      */
-    public function init() {
-        $this->title = get_string( 'pluginname', 'block_leeloolxp_synchronizer' );
-
+    function init() {
+        // Commented dynamic call for now.
+        $this->title = get_string('pluginname', 'block_leeloolxp_synchronizer');
     }
 
-    /**
-     * Return contents of block_leeloolxp_synchronizer block
-     *
-     * @return string of block
-     */
-    public function  get_content() {
+  
 
-        if ( $this->content !== null ) {
-
+    function get_content() {
+        if ($this->content !== NULL) {
             return $this->content;
-
         }
 
-        if ( empty( $this->instance ) ) {
-
+        if (empty($this->instance)) {
             $this->content = '';
+            return $this->content;
+        }
+
+            global $PAGE;
+
+            global $DB;
+
+            global $CFG;
+
+            if($PAGE->pagetype=='course-view-topics') {
+
+                $liacencekey = get_config('block_leeloolxp_synchronizer')->leeloolxp_block_synchronizer_licensekey;
+
+                $courseid  = $_REQUEST['id'];
+
+                $alreadysync = false;
+
+                $baseurl = $CFG->wwwroot;
+
+                $coursesyncedquery = $DB->get_records('tool_leeloolxp_sync',
+
+                array('courseid' => $courseid));
+
+                ?><style type="text/css">
+                        .dialog-modal {
+align-self: center;
+position: fixed;
+top: 0;
+left: 0;
+width: 100%;
+height: 100%;
+z-index: 9999;
+background: rgba(0,0,0,0.7);
+display: flex;
+align-items: center;
+justify-content: center;
+}
+.dialog-modal-inn {
+background: #fff;
+max-width: 750px;
+padding: 50px;
+text-align: center;
+width: 100%;
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+}
+.dialog-modal-inn div#dialog {
+font-size: 17px;
+}
+.dialog-modal-inn h4 {
+font-weight: 400;
+margin: 0 0 25px;
+font-size: 25px;
+}
+.sure-btn {
+margin: 50px 0 0;
+}
+.dialog-modal-inn .sure-btn button {
+font-size: 20px;
+padding: .5rem 3rem;
+color: #fff;
+background-color: #74cfd0;
+border: none;
+display: inline-block;
+text-decoration: none;
+outline: 0;
+box-shadow: none;
+margin: 10px 0;
+}
+                    </style> <?php
+
+                if (empty($coursesyncedquery)) {
+
+                    $html = '<div id="dialog-modal-course-synchronizer" class="dialog-modal dialog-modal-course " style="display: none;">
+
+                            <div class="dialog-modal-inn">
+
+                                <div id="dialog" >
+
+                                    <h4>Are you sure you want to sync all the activities and resources of this course to Leeloo LXP?</h4>
+
+                                    <div class="sure-btn">
+
+                                        <button data_id = "" data_name="" onclick="yescourseunsync('.$courseid.');" 
+
+                                        class="btn btn_yes_courseunsync" >Yes, I’m sure</button>
+
+                                        <button  onclick="course_cls_popup();" class="btn course_cls_popup" >Cancel</button>
+
+                                    </div>
+
+                                    
+
+                                </div>
+
+                            </div>
+
+                        </div>';
+
+                } else {
+
+                    $html = '<div id="dialog-modal-course-synchronizer" class="dialog-modal dialog-modal-course " style="display: none;">
+
+                            <div class="dialog-modal-inn">
+
+                                <div id="dialog" >
+
+                                    <h4>Are you sure you want to RE-sync all the activities and resources of this course to Leeloo LXP?</h4>
+
+                                    <div class="sure-btn">
+
+                                        <button data_id = "" data_name="" onclick="resync('.$courseid.');" 
+
+                                        class="btn btn_yes_courseunsync" >Yes, I’m sure</button>
+
+                                        <button  onclick="course_cls_popup();" class="btn course_cls_popup" >Cancel</button>
+
+                                    </div>
+
+                                   
+
+                                </div>
+
+                            </div>
+
+                        </div>';
+
+                }
+
+                if(isset($_REQUEST['sync'])) {
+
+                    $html .= '<p style="color:green;">Sychronizationed successfully.</p>';    
+
+                }
+
+                $html .= "<h2>Synchronizer To leeloolxp</h2>";
+
+                $html .= "<hr>";
+
+                $html .= "<a href='#' onclick='show_popup();'>Sync Course </a><br>";
+
+                $html .= "<a href='#' onclick='single_activity(".$courseid.");'>Sync Single Activity</a><br>";
+
+                $html .= "<a href='#' onclick='sync_categories(".$courseid.");'>Sync Categories</a>";
+                
+                $html .= '<script> function show_popup() {
+
+                        document.getElementById("dialog-modal-course-synchronizer").style.display = "block";
+
+                    } 
+
+                
+
+                function course_cls_popup() {
+
+                    document.getElementById("dialog-modal-course-synchronizer").style.display = "none";
+
+                }
+                function yescourseunsync(courseid) {
+
+                    var url = "'.$baseurl.'/admin/tool/leeloolxp_sync/?action=coursesyncfrmblock&redirect=couseview&courseid="+courseid;
+
+                    window.location = url;
+
+                }
+
+                function resync(courseid) {
+
+                    
+
+                    var url = "'.$baseurl.'/admin/tool/leeloolxp_sync/?resync=1&redirect=courseview&courseid_resync="+courseid;
+
+                    window.location = url;
+
+                }
+
+                function single_activity(courseid) {
+
+                    var url = "'.$baseurl.'/admin/tool/leeloolxp_sync/?action=add&redirect=couseview&courseid="+courseid;
+
+                    window.location = url;
+
+                }
+
+                function sync_categories(courseid) {
+                    var url = "'.$baseurl.'/admin/tool/leeloolxp_sync/?syncategory=1&redirect=couseview&courseid="+courseid;
+                    window.location = url;
+                }
+
+                </script>';
+
+            }
+
+            $this->content->text = $html;
+
+            $this->content->footer = '';
 
             return $this->content;
-
-        }
-        global $DB;
-        global $CFG;
-        if ( $this->page->pagetype == 'course-view-topics' ) {
-            $liacencekey = get_config( 'block_leeloolxp_synchronizer' )
-            ->licensekey;
-            $courseid  = $_REQUEST['id'];
-            $alreadysync = false;
-            $baseurl = $CFG->wwwroot;
-            $coursesyncedquery = $DB->get_records( 'tool_leeloolxp_sync',
-            array( 'courseid' => $courseid ) );
-
-            if ( empty( $coursesyncedquery ) ) {
-                $html = '<div id="dialog-modal-course-synchronizer"
-                class="dialog-modal dialog-modal-course " style="display: none;">
-                            <div class="dialog-modal-inn">
-                                <div id="dialog" >
-                                    <h4>Are you sure you want to sync all the activities
-                                    and resources of this course to Leeloo LXP?</h4>
-                                    <div class="sure-btn">
-                                        <button data_id = "" data_name=""
-                                        onclick="yescourseunsync('.$courseid.');"
-                                        class="btn btn_yes_courseunsync" >Yes, I’m sure</button>
-                                        <button  onclick="course_cls_popup();" class="btn course_cls_popup" >Cancel</button>
-                                    </div>
-                                    </div>
-                            </div>
-                        </div>';
-            } else {
-                $html = '<div id="dialog-modal-course-synchronizer"
-                class="dialog-modal dialog-modal-course " style="display: none;">
-                            <div class="dialog-modal-inn">
-                                <div id="dialog" >
-                                    <h4>Are you sure you want to RE-sync all the activities
-                                    and resources of this course to Leeloo LXP?</h4>
-                                    <div class="sure-btn">
-                                        <button data_id = "" data_name="" onclick="resync('.$courseid.');"
-                                        class="btn btn_yes_courseunsync" >Yes, I’m sure</button>
-                                        <button  onclick="course_cls_popup();" class="btn course_cls_popup" >Cancel</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>';
-            }
-            if ( isset( $_REQUEST['sync'] ) ) {
-                $html .= '<p style="color:green;">Sychronizationed successfully.</p>';
-
-            }
-            $html .= '<h2>Synchronizer To leeloolxp</h2>';
-            $html .= '<hr>';
-            $html .= "<a href='#' onclick='show_popup();'>Sync Course </a><br>";
-            $html .= "<a href='#' onclick='single_activity(".$courseid.");'>Sync Single Activity</a>";
-            $html .= '<script> function show_popup() {
-                        document.getElementById("dialog-modal-course-synchronizer").style.display = "block";
-                    }
-                    function course_cls_popup() {
-                    document.getElementById("dialog-modal-course-synchronizer").style.display = "none";
-                }
-
-                function yescourseunsync(courseid) {
-                    var url = "'.$baseurl.'/admin/tool/leeloolxp_sync/
-                    ?action=coursesyncfrmblock&redirect=couseview&courseid="+courseid;
-                    window.location = url;
-                }
-                function resync(courseid) {
-                    var url = "'.$baseurl.'/admin/tool/leeloolxp_sync/
-                    ?resync=1&redirect=courseview&courseid_resync="+courseid;
-                    window.location = url;
-                }
-                function single_activity(courseid) {
-                    var url = "'.$baseurl.'/admin/tool/leeloolxp_sync/
-                    ?action=add&redirect=couseview&courseid="+courseid;
-                    window.location = url;
-                }
-                </script>';
-        }
-        $this->content->text = $html;
-        $this->content->footer = '';
-        return $this->content;
-
     }
 
-    /**
-     * Allow the block to have a configuration page
-     *
-     * @return boolean
-     */
-    public function has_config() {
+   
+
+    function has_config() {
+
         return true;
+
     }
+
 }
