5ea121f621a5d3722bf456ec17e6be5145e010af
Anay <anay.infowind@gmail.com>
2021-03-10 17:41:13 +0530

    standard capability block/leeloolxp_synchronizer:myaddinstance

--- db/access.php
+++ db/access.php
@@ -25,6 +25,16 @@ defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
 
+    'block/leeloolxp_synchronizer:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW,
+        ),
+
+        'clonepermissionsfrom' => 'moodle/my:manageblocks',
+    ),
+
     'block/leeloolxp_synchronizer:addinstance' => array(
         'riskbitmask' => RISK_SPAM | RISK_XSS,
 
